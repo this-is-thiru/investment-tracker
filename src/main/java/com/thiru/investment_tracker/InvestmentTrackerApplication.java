ae256c616ede886bebbc54a1f07c4b12870978e8
Thiru <pthirumalayadav@gmail.com>
2024-06-08 17:19:42 +0530

    Add transaction management

--- src/main/java/com/thiru/investment_tracker/InvestmentTrackerApplication.java
+++ src/main/java/com/thiru/investment_tracker/InvestmentTrackerApplication.java
@@ -3,6 +3,8 @@ package com.thiru.investment_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -18,5 +20,10 @@ public class InvestmentTrackerApplication {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+		return new MongoTransactionManager(dbFactory);
 	}
 }
