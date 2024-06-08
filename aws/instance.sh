f9f91ba31f0a496794cee51ecd97e580ed393548
Thiru <pthirumalayadav@gmail.com>
2024-06-08 22:43:57 +0530

    rename to runbook daily

--- aws/instance.sh
+++
@@ -1,7 +0,0 @@
-# start docker daemon
-sudo service docker start
-sudo chmod 666 /var/run/docker.sock
-
-# start github self hosted runner
-./config.sh --url https://github.com/this-is-thiru/investment-tracker --token AQPGMTD56CBDC5ZV4CLLTVLGMSNNK
-./run.sh
