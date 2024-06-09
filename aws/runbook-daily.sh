fd36083aee8b24be12757e79a7a3f99628674b4c
Thiru <pthirumalayadav@gmail.com>
2024-06-09 16:59:41 +0530

    aws commands

--- aws/runbook-daily.sh
+++ aws/runbook-daily.sh
@@ -2,6 +2,10 @@
 sudo service docker start;
 sudo chmod 666 /var/run/docker.sock
 
+# run the docker container
+docker rm -f investment-tracker-container;
+docker run -d -p 8080:8080 --name investment-tracker-container tperamasani/investment-tracker
+
 # start github self hosted runner
 ./config.sh --url https://github.com/this-is-thiru/investment-tracker --token AQPGMTD56CBDC5ZV4CLLTVLGMSNNK;
 ./run.sh
