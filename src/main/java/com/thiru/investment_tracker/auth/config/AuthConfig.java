994b24c7cf4d0af45c9497531d7df45b873f60f2
Peramasani Thirumala Yadav <pthirumalayadav@gmail.com>
2025-12-13 22:21:46 +0530

    log annotation

--- src/main/java/com/thiru/investment_tracker/auth/config/AuthConfig.java
+++ src/main/java/com/thiru/investment_tracker/auth/config/AuthConfig.java
@@ -17,11 +17,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +29,7 @@ public class AuthConfig {
     private final UserDetailsService userDetailsService;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
