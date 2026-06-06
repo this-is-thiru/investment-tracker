b747126ad624e1becdad8954b1f969c61cca3731
Peramasani Thirumala Yadav <pthirumalayadav@gmail.com>
2026-06-06 19:19:30 +0530

    fix: remove duplicate transactionManager bean definition

--- src/main/java/com/thiru/investment_tracker/config/InvestmentTrackerConfig.java
+++ src/main/java/com/thiru/investment_tracker/config/InvestmentTrackerConfig.java
@@ -2,8 +2,6 @@ package com.thiru.investment_tracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,10 +11,5 @@ public class InvestmentTrackerConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
-		return new MongoTransactionManager(dbFactory);
 	}
 }
