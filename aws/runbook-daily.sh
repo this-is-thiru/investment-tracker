d94abc6b071493260afbf788950a7a125b83a971
Peramasani Thirumala Yadav <pthirumalayadav@gmail.com>
2026-06-21 17:49:03 +0530

    Update GitHub URLs and remote for repo rename to wealthlens
    - Update origin remote URL: git@github.com:this-is-thiru/wealthlens.git
    - Update self-hosted runner config URL in aws/runbook-daily.sh

--- aws/runbook-daily.sh
+++ aws/runbook-daily.sh
@@ -7,5 +7,5 @@ docker rm -f wealthlens-container;
 docker run -d -p 8080:8080 --name wealthlens-container tperamasani/wealthlens
 
 # start github self hosted runner
-./config.sh --url https://github.com/this-is-thiru/investment-tracker --token AQPGMTD56CBDC5ZV4CLLTVLGMSNNK;
+./config.sh --url https://github.com/this-is-thiru/wealthlens --token AQPGMTD56CBDC5ZV4CLLTVLGMSNNK;
 ./run.sh
